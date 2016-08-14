dcf2c77f065893aacd788821d801c39bb74290aa
CK <speakerdamage@gmail.com>
2016-08-14 16:12:44 -0400

    post animation css

--- js/svg.js
+++ js/svg.js
@@ -1,7 +1,8 @@
 new Vivus('vanguard-svg', {
-    type: 'delayed',
+    type: 'async',
     duration: 100,
     animTimingFunction: Vivus.EASE
 }, function(){
   console.log("vanguard animation done");
+  $(".wrapper").css("background-color", "#222");
 });
